8d3ed2bdec75ad937ac5bec3c5cf82b05fb9a0a3
hunter grubbs <hunter.grubbs07@gmail.com>
2023-05-02 23:02:56 -0500

    added nav bar dynamic styling

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// home page
+const exploreCircle = document.querySelector('.explore-circle');
+const exploreCircleHover = document.querySelector('#explore-circle-hover');
+
+exploreCircle.addEventListener('mouseover', (e)=>{
+    console.log(e);
+});
